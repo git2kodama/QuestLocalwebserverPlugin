37f9d6c22f4e0e9d5263b2b9e32d538d168506e2
git2kodama <ebook2kodama@gmail.com>
2020-11-10 22:46:09 +0900

    nonohttpdを加えたlocalwebserver

--- WebServer/src/main/java/com/kodama/webserver/NativeLocalWebServer.java
+++ WebServer/src/main/java/com/kodama/webserver/NativeLocalWebServer.java
@@ -1,15 +1,21 @@
 package com.kodama.webserver;
-import android.util.Log;
+
 import com.unity3d.player.UnityPlayer;
 
-public class NativeLocalWebServer {
-    public NativeLocalWebServer() {
-        Log.i("Unity","ネィティブ");
+import java.io.IOException;
+
+import fi.iki.elonen.NanoHTTPD;
+
+public class NativeLocalWebServer extends NanoHTTPD {
+
+    public NativeLocalWebServer() throws IOException {
+        super(2921);
+        start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
+        debug_message("Running! Point your browsers to http://localhost:8080/ \n");
     }
 
-    public void debug_message(){
-        UnityPlayer.UnitySendMessage("webserver", "", "");
-        Log.i("Unity","debug_message");
+    public void debug_message(String _message){
+        UnityPlayer.UnitySendMessage("webserver","android_debug_log", _message);
     }
 
 }
